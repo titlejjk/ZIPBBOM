73c63fac2352ccb9b47556625849aecd03e1c626
saeunnnnni <kate6011@naver.com>
2023-07-20 19:41:06 +0900

    test

---
+++ src/main/java/com/project/blog/ZipbbomApplication.java
@@ -0,0 +1,13 @@
+package com.project.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipbbomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipbbomApplication.class, args);
+	}
+
+}
